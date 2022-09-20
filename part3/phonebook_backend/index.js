060c93e1308228858dfc8e2eaf36a607855e3c94
Yiju <voi224051.sc09@nycu.edu.tw>
2022-09-20 16:19:02 +0800

    Completed 3.2

---
+++ part3/phonebook_backend/index.js
@@ -0,0 +1,65 @@
+const { response } = require('express')
+const express = require('express')
+const app = express()
+
+let notes = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+  
+app.get('/api/persons', (req, res) => {
+    res.json(notes)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const note = notes.find(note => note.id === id)
+    
+    if (note) {    
+        res.json(note)  
+    } else {    
+        res.status(404).end()  
+    }
+})
+
+app.get('/info', (req, res) => {
+    const d = new Date();
+    res.send(`
+        <p>Phonebook has info for ${notes.length} people.</p>
+        <p>${d.toDateString()} ${d.toTimeString()}</p>
+    `)
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id)
+    notes = notes.filter(note => note.id !== id)
+  
+    res.status(204).end()
+})
+  
+const PORT = 3001
+    app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
